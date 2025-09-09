cd88b8d45a069caacbefc52b639aba6ed6cb09af
ccf-2012 <crlf66@163.com>
2025-09-09 08:34:16 +0800

    clean

--- backend/run.py
+++ backend/run.py
@@ -3,7 +3,8 @@ import os
 from dotenv import load_dotenv
 
 # Load .env file from the project root, which is two levels up
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '..', '.env'))
+# load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '..', '..', '.env'))
+load_dotenv()
 
 if __name__ == "__main__":
     # 从环境变量获取端口，或使用默认值 6006
