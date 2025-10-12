937667a3b2b5ba34881acb6bb0d34da4f73f06d6
ccf-2012 <crlf66@163.com>
2025-10-12 15:49:04 +0800

    dual stack listen

--- backend/run.py
+++ backend/run.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         "app.main:app",
-        host="0.0.0.0",
+        host=["::", "0.0.0.0"],
         port=port,
         reload=False,
         workers=1
